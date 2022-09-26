6dfea864d9ca964e389ae135f647d4e188b2a4df
Lilywd <ndungulilianwanjiku@gmail.com>
2022-09-26 18:02:32 +0300

    updates

--- Qrcode/urls.py
+++ Qrcode/urls.py
@@ -30,11 +30,11 @@ urlpatterns = [
 
     # path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password/password_reset_done.html'),name='password_reset_done'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password/password_reset_done.html'),name='password_reset_done'),
 
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password/password_reset_confirm.html'), name='password_reset_confirm'),
     
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password/password_reset_complete.html'),name='password_reset_complete'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password/password_reset_complete.html'),name='password_reset_complete'),
 
     path('oauth/', include('social_django.urls',namespace='social')),
 
