84814f7d43575b14f75e1f02c30f57a48b03a1be
Peter Evance <peterevance1@gmail.com>
2022-10-10 07:28:42 +0300

    User manager test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from users import models
 
-# Create your tests here.
+class TestModels(TestCase):
+    def test_my_user_manager_works(self):
+        models.User.objects.create(
+            username = "lilian-wanjiku",
+            first_name= "lilian",
+            last_name = "wanjiku",
+            email = "lilianwanjiku@gmail.com")
+        self.assertEqual(len(models.User.objects.all()), 1)
